ea10f2a0f631b6a07ae13f9eed606955924af037
Jinesh <jineshpaloor@gmail.com>
2015-08-19 17:12:46 +0530

    save lda model to file

--- tag_wikipedia_articles.py
+++ tag_wikipedia_articles.py
@@ -6,6 +6,7 @@ import logging
 logging.basicConfig(filename='wiki.log', level=logging.INFO)
 
 DICT_PATH = "/tmp/wiki.dict"
+MODEL_PATH = "/tmp/wiki.lda"
 
 
 class TagWiki(object):
@@ -31,10 +32,15 @@ class TagWiki(object):
             content = gensim.parsing.preprocess_string(page.content)
         
             self.dictionary.add_documents([title, content])
+        self.dictionary.save(DICT_PATH)
         return True
     
     # Pass 2: Process topics
     def process_topics(self):
+        if os.path.exists(MODEL_PATH):
+            self.lda = gensim.models.ldamodel.LdaModel.load(MODEL_PATH)
+            return True
+
         self.lda = gensim.models.ldamodel.LdaModel(
             corpus=None, id2word=self.dictionary, num_topics=30, 
             update_every=1, chunksize=1, passes=2)
@@ -49,19 +55,18 @@ class TagWiki(object):
                 content_bow = self.dictionary.doc2bow(content)
             
                 new_bag_of_words = title_bow + content_bow
-                logging.info('Content BOW :{0}'.format(content_bow))
                 self.lda.update([content_bow])
             
-                logging.info("{0}: {1}".format(link, self.lda[new_bag_of_words]))
+                logging.info("RESULT: {0}: {1}".format(link, self.lda[new_bag_of_words]))
             except:
                 logging.info("{0}: PROCESSING FAILED!".format(link))
+        self.lda.save(MODEL_PATH)
         return True
     
 def main():
     wiki = TagWiki()
     logging.info("No. of keys at start :{0}".format(wiki.dictionary.keys().__len__()))
     wiki.prepare_dictionary()
-    wiki.dictionary.save(DICT_PATH)
     logging.info("No. of keys after addition of data {0}".format(wiki.dictionary.keys().__len__()))
     wiki.process_topics()
     logging.info("done : {0}".format(wiki.dictionary.__sizeof__()))
